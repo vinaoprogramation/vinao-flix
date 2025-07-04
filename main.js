b5d6bba4846024e0b1138635b1125a6aaf21dc90
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-07-04 10:48:32 -0300

    q

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function inserirNome(){
     let nomeUsuario = prompt("Tu nasceu com que nome? ");
     let elemento = document.querySelector("#nome-usuario");
+    elemento.textContent = nomeUsuario;
 }
